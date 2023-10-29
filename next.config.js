eebbc70ffe115b7c63b8ee9aaf0df71e6fb3af9f
SID12g <sid12g@naver.com>
2023-10-30 08:02:57 +0900

    retry 4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath:"/{sid12g-blog}"
-}
+const debug = process.env.NODE_ENV !== "production";
+const repository = "sid12g-blog";
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+};
+
+module.exports = nextConfig;
