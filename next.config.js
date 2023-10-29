cb08293552eacfd52ad9166aa17ba07d6143e0d1
SID12g <sid12g@naver.com>
2023-10-30 08:27:17 +0900

    retry 9

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const debug = process.env.NODE_ENV !== "production";
-const repository = "sid12g-blog";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://sid12g.github.io/sid12g-blog"
-      : "", 
-    trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
-    output: 'export'
-};
+    basePath:"/{sid12g-blog}"
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
