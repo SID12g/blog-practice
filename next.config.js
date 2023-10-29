3e51d7c697d4d231a28f34e7411cb88fda9119fe
SID12g <sid12g@naver.com>
2023-10-30 08:14:59 +0900

    retry 8

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const repository = "sid12g-blog";
 
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+    assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://sid12g.github.io/sid12g-blog"
+      : "", 
     trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
     output: 'export'
 };
