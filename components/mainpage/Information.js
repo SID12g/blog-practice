29c60eed347d37f0cf2c9a4d90aecab0bdc32646
SID12g <sid12g@naver.com>
2023-10-30 10:47:28 +0900

    rerererere

--- components/mainpage/Information.js
+++ components/mainpage/Information.js
@@ -2,15 +2,17 @@ import Image from "next/image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import CloudBg from '@/public/Image/CloudBg.png'
+import exProfile from '@/public/Image/ex-profile.jpeg'
 import Link from "next/link";
 import LoginBtn from "./LogInBtn";
 export default function Information() {
     return (
         <div className="information">
             <div className="wrap-bg">
-                <Image src="/images/CloudBg.png" alt="background-image" fill={true} style={{ filter: "brightness(50%)" }} />
+                <Image src={CloudBg} alt="background-image" fill={true} style={{ filter: "brightness(50%)" }} />
                 <div className="profile-img">
-                    <Image src="/images/ex-profile.jpeg" alt="profile-image" fill={true} style={{ borderRadius: 100 }} />
+                    <Image src={exProfile} alt="profile-image" width={100} height={100} style={{ borderRadius: 100 }} />
                 </div>
                 <div className="my-name">
                     <h3>Sungmin Cho</h3>
@@ -22,7 +24,7 @@ export default function Information() {
                         </div>
                         <p className="des-text">sid12g@naver.com</p>
                     </div>
-                    <Link href="">
+                    <Link href="https://github.com/SID12g">
                         <div className="my-github">
                             <div className="des-icon">
                                 <FontAwesomeIcon icon={faGithub} style={{ color: "#fffff", width: "20px" }} />
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath:"/sid12g-blog"
+    basePath:"/sid12g-blog",
+    images: {
+        domains: ["localhost", "*"],
+      },
+      reactStrictMode: true,
 }
 
 module.exports = nextConfig
