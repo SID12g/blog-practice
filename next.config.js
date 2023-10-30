1a0ebb8a985eb3d36309cc80f8607e91f946e6fb
SID12g <sid12g@naver.com>
2023-10-30 10:58:57 +0900

    birea

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath:''
+    basePath:'/'
 }
+module.exports = {
+    poweredByHeader: process.env.NODE_ENV === 'development',
+    reactStrictMode: process.env.NODE_ENV === 'development',
+  }
 
 module.exports = nextConfig
