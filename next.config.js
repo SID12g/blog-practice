3134de33e922f65b497c5aeb0ad1ff0cc2c95728
SID12g <sid12g@naver.com>
2023-10-30 12:00:41 +0900

    yoyo

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     basePath: '',
     poweredByHeader: process.env.NODE_ENV === 'development',
     reactStrictMode: process.env.NODE_ENV === 'development',
-  };
-  
-  module.exports = nextConfig;
-  
+};
+
+module.exports = nextConfig;
