0b000c256ab078d4985502e77dc3a57747129046
SID12g <sid12g@naver.com>
2023-10-30 12:15:03 +0900

    config nor

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '',
-    poweredByHeader: process.env.NODE_ENV === 'development',
-    reactStrictMode: process.env.NODE_ENV === 'development',
-};
-
-module.exports = nextConfig;
+    /* config options here */
+  }
+   
+  module.exports = nextConfig
